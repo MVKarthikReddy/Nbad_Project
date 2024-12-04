5b1887c36e01e13116af21d7dbfee16e7a6d204a
MVKarthikReddy <karthikmedagam@gmail.com>
2024-12-05 03:08:36 +0530

    finished

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,6 +6,7 @@ dotenv.config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
+    // console.log(process.env.MONGODB_URI)
     console.log('Connected to MongoDB');
   } catch (error) {
     console.error('MongoDB connection error:', error);
